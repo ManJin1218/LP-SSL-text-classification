926cc058dd722f44b5d32bbb98825ada7a2b70b6
Man Jin <32991904+ManJin1218@users.noreply.github.com>
2020-04-15 21:53:53 -0400

    Add early stopping

--- train_phase2.py
+++ train_phase2.py
@@ -20,10 +20,13 @@ def main():
     parser.add_argument("--hidden_dim", default=32, type=int, help="hidden dim")
     parser.add_argument("--num_layers", default=2, type=int, help="number of layers for GRU")
     parser.add_argument("--embedding_dim", default=300, type=int, help="embedding dim")
+    parser.add_argument("--seed", default=1211, type=int, help="random seed")
     args = parser.parse_args()
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    torch.manual_seed(88)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
 
     # load dataloaders
     with open("data_local/processed/data_{}_{}.pickle".format(args.model_type, args.num_labeled), 'rb') as handle:
@@ -45,10 +48,18 @@ def main():
     criterion = nn.CrossEntropyLoss(reduction="none")
     optimizer = optim.Adam(model.parameters())
     print("Epoch 0")
-    train(pseudo_loader, d["val_loader"], model, optimizer, criterion, device, args)
+    loss, accuracy = train(pseudo_loader, d["val_loader"], model, optimizer, criterion, device, args)
 
     # path of the phase2 model
     PATH = "models/{}_{}_model.pt".format(args.name, args.model_type)
+    
+    train_loss_history = []
+    val_accuracy_history = []
+    train_loss_history.append(loss)
+    val_accuracy_history.append(accuracy)
+    best_val_acc = 0
+    early_stop = 5
+    early_stop_cnt = 0
 
     # epoch 1-T'
     for i in range(args.total_epochs):
@@ -57,11 +68,26 @@ def main():
         p_labels, updated_weights, updated_class_weights = label_propagation(batch_features, d["groundtruth_labels"], d["labeled_idx"], d["unlabeled_idx"], k=args.knn)
         pseudo_loader = update_pseudoloader(d["all_indices"], p_labels, updated_weights, updated_class_weights)
         model = create_model(model_config, phase2=True)
-       model.load_state_dict(torch.load(PATH, map_location=torch.device(device))["model_state_dict"])
+        model.load_state_dict(torch.load(PATH, map_location=torch.device(device))["model_state_dict"])
         model = model.to(device)
         criterion = nn.CrossEntropyLoss(reduction="none")
         optimizer = optim.Adam(model.parameters())
-        train(pseudo_loader, d["val_loader"], model, optimizer, criterion, device, args)
+        loss, accuracy = train(pseudo_loader, d["val_loader"], model, optimizer, criterion, device, args)
+        train_loss_history.append(loss)
+        val_accuracy_history.append(accuracy)
+        if accuracy > best_val_acc:
+            early_stop_cnt = 0
+            best_val_acc = accuracy
+            torch.save({
+                "model_state_dict": model.state_dict(),
+                "train_loss_history": train_loss_history,
+                "val_accuracy_history": val_accuracy_history,
+                "args": args
+            }, "models/{}_{}_{}_model.pt".format(args.name, args.model_type, args.num_labeled))
+        else:
+            early_stop_cnt += 1
+            if early_stop_cnt >= early_stop:
+                return
 
 
 if __name__ == "__main__":
